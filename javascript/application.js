2522ee29ff67373503cf2f29b91d58b3ba5687ea
John Cleary <john@createk.io>
2020-05-26 22:09:43 +0100

    printable monster tags with QR codes

--- javascript/application.js
+++ javascript/application.js
@@ -1,37 +1,39 @@
 function getUrlVars()
 {
-  var vars = [], hash;
-  var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-  for(var i = 0; i < hashes.length; i++)
+  let vars = [], hash;
+  let hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+  for(let i = 0; i < hashes.length; i++)
   {
     hash = hashes[i].split('=');
     vars.push(hash[0]);
     vars[hash[0]] = hash[1];
   }
   return vars;
-};
+}
 
-function showMonsters() {
-  let i = 0;
+function showMonsters(perRow = 2) {
   let content = '';
+  let colClass = 'col-' + (12 / perRow);
 
-  for (i = 1; i <= 12; i++) {
-    if ((i % 2) == 1) {
+  for (let i = 0; i < 12; i++) {
+    if ((i % perRow) == 0) {
       content += '<div class="row">';
     }
 
     content += '\
-      <div class="col-6"> \
+      <div class="' + colClass + '"> \
         <div class="card" > \
-          <div id="monster-' + i + '-lock" class="locked"></div> \
-          <img src="images/mon-' + i + '.jpeg" class="card-img-top monster-img"> \
-          <div class="card-body"> \
-            <p class="card-text">Monster ' + i + '</p> \
+          <div id="monster-' + (i+1) + '-lock" class="locked"></div> \
+          <img src="images/mon-' + (i+1) + '.jpeg" class="card-img-top monster-img"> \
+          <div class="card-body text-center"> \
+            <p class="card-text">Monster ' + (i+1) + '</p> \
+            <div id="monster-' + (i+1) + '-card-body" class="text-center"></div> \
           </div> \
         </div> \
       </div> \
-      ';
-    if ((i % 2) == 0) {
+    ';
+
+    if ((i % perRow) == (perRow - 1)) {
       content += '</div>';
     }
   }
